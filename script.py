2620a080a8b786e27d3fdabd79ce6be15298f10b
Jonathan Gao <jonathangao008@gmail.com>
2026-06-21 14:19:32 -0400

    Successfully reqest the html with the 'requests.get()' function and turn it into a BeautifulSoup object.

--- script.py
+++ script.py
@@ -1,1 +1,15 @@
-print("Hello, World!")
+import requests
+from bs4 import BeautifulSoup
+import lxml
+
+response = requests.get("https://supremevalues.com/mm2/godlies")
+print(response)
+
+soup = BeautifulSoup(response.text, "lxml")
+title = soup.title
+print(title.text)
+
+if "Blossom" in response.text:
+    print("Blossom is in the response")
+else:
+    print("Blossom is not in the response")
