aa328d147306bf4f4c7b3547eaf0a9c6354da716
Jonathan Gao <jonathangao008@gmail.com>
2026-07-05 20:15:48 -0400

    Saved file before adding Surface Laptop to devices with access.

--- app.py
+++ app.py
@@ -1,36 +1,7 @@
-import os
-import sqlite3
+from flask import Flask, render_template
 
-import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
+app = Flask(__name__)
 
-
-st.set_page_config(layout="wide")
-
-st.title("MM2 Values Plus")
-
-st.write("This is a tool to help you track the values of your weapons in the game Murder Mystery 2.")
-
-st.image(os.path.join(os.getcwd(), "static", "MM2Logo"))
-
-st.divider()
-
-connection = sqlite3.connect("weapons.db")
-cursor = connection.cursor()
-
-df = pd.read_sql("SELECT * FROM godlies", connection)
-uniqueNames = df["name"].unique()
-
-selectedWeapon = st.selectbox("Select a weapon", uniqueNames)
-
-weaponData = df[df["name"] == selectedWeapon]
-weaponData = weaponData.sort_values(by="createdAt", ascending=True)
-weaponData["createdAt"] = pd.to_datetime(weaponData["createdAt"]).dt.strftime("%Y-%m-%d")
-
-st.dataframe(weaponData)
-st.line_chart(weaponData, x="createdAt", y="value", height=500)
-
-
-
-connection.close()
+@app.route("/")
+def index():
+    return render_template("test.html")
